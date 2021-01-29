558547e979710028c9b555e877f9fc4476d63ff0
Luis <luis_felipe_fc@hotmail.com>
2021-01-29 11:04:23 -0300

    Upload

---
+++ fist-classl-function.js
@@ -0,0 +1,20 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b
+}
+
+const multply = function(a, b) {
+    return a * b
+}
+
+const divide = function(a, b) {
+    return a / b
+}
+
+console.log(add(10, 20))
+console.log(subtract(10, 20))
+console.log(multply(10, 20))
+console.log(divide(10, 20))
